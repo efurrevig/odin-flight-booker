fef61f1cf0f59986a725556e928f1159513e817f
Eric Furrevig <efurrevig@gmail.com>
2023-01-21 16:42:52 -0500

    Airport/Flight departing_flights/arriving_flights associations + flight search form at flights#index

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,22 @@
 class FlightsController < ApplicationController
+
+    def index
+        @airports = Airport.all
+        @flights = Flight.all
+        
+        # Filter flights by params if present
+        if params[:departure_airport].present?
+            @flights = @flights.where(departure_airport: params[:departure_airport])
+        end
+
+        if params[:arrival_airport].present?
+            @flights = @flights.where(arrival_airport: params[:arrival_airport])
+        end
+
+        if params[:departure_time].present?
+            @flights = @flights.where(departure_time: params[:departure_time])
+        end
+
+    end
+
 end
